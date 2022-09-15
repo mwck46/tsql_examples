0bfde304daeafa69c192efaac39b9708a8aa89a6
Michael Wong <34909192+mwck46@users.noreply.github.com>
2022-09-15 14:49:37 +0800

    Update case_when.sql

--- case_when.sql
+++ case_when.sql
@@ -1,6 +1,14 @@
-select employee_id, bonus =  
-      CASE   
-         WHEN (employee_id % 2) != 0 AND name NOT LIKE 'M%' THEN salary
-         ELSE 0
-	  END
-from Employees;
+SELECT employee_id, bonus =  
+  CASE   
+    WHEN (employee_id % 2) != 0 AND name NOT LIKE 'M%' THEN salary
+    ELSE 0
+  END
+FROM Employees;
+
+
+UPDATE salary
+SET sex = 
+  CASE 
+    WHEN sex = 'm' THEN 'f'
+    ELSE 'm'
+  END;
